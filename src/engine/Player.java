056f2113c98febc67146071e6c27578950b923a7
Burak Erdem Varol <burakerdemvarol@gmail.com>
2018-12-21 20:48:32 +0300

    Folder name changed!

---
+++ src/engine/Player.java
@@ -0,0 +1,51 @@
+package control;
+
+
+public class Player {
+
+	//Attributes
+	
+	int score;
+	String name;
+	boolean isTurn;
+	
+	
+	//Constructors
+	
+	Player(String nameOfPlayer)
+	{
+		name = nameOfPlayer;
+		score = 0;
+		isTurn = false;
+	}
+	
+	//Methods
+
+	public int getScore()
+	{
+		return score;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public void setScore(int newScore)
+	{
+		score = newScore;
+	}
+	
+	public void setName (String newName)
+	{
+		name = newName;
+	}
+	
+	public void changeTurn ()
+	{
+		if (isTurn)
+			isTurn = false;
+		else
+			isTurn = true;
+	}
+}
