146279e87ae0acf5608b58a452cd8ab7ff1705da
Mahmud Sami Aydın <mahmudsami97@gmail.com>
2018-12-21 21:29:29 +0300

    Update GameView.java

--- src/view/GameView.java
+++ src/view/GameView.java
@@ -1,6 +1,6 @@
-package vgameView;
+package gameView;
 import java.io.FileNotFoundException;
-
+import java.lang.Math;
 import control.GameEngine;
 import javafx.application.Application;
 import javafx.scene.*;
@@ -15,6 +15,17 @@ import javafx.scene.transform.*;
 
 public class GameView extends Application
 {
+	public static String levelX;
+	public GameView( )
+	{
+		super();
+	}
+	public GameView( String levelX )
+	{
+		super();
+		this.levelX = levelX;
+	}
+
    public static Sphere[][][] nodes = new Sphere [GameEngine.HEIGHT][GameEngine.WIDTH][GameEngine.DEPTH]  ;
    
    public Scene createScene() {
@@ -29,7 +40,7 @@ public class GameView extends Application
       bound2.getTransforms().add(new Rotate(90,Rotate.Y_AXIS));
       
       Rectangle bound3 = new Rectangle( 0, 0, GameEngine.DEPTH, GameEngine.HEIGHT );      
-      bound3.setFill(Color.BLACK);
+      bound3.setFill(Color.AQUA);
       bound3.getTransforms().add(new Rotate(90,Rotate.X_AXIS));
       bound3.getTransforms().add(new Rotate(270,Rotate.Z_AXIS));
       root.getChildren().add(bound);
@@ -39,12 +50,13 @@ public class GameView extends Application
       PhongMaterial tempMaterial;
       //-----------------------------------------------------------------------------       
       
-      for( int i = 0; i < GameEngine.HEIGHT; i++)
+      for( int k = GameEngine.DEPTH - 1; k >=0 ; k--)
       {
          for( int j = 0; j < GameEngine.WIDTH; j++)
          {
-            for( int k = 0; k < GameEngine.DEPTH; k++)
+            for( int i = GameEngine.HEIGHT - 1 ; i >= 0 ; i-- )
             {
+            	if( (k + i) % 2 == 0 && ( j+i ) % 2 == 0 ){
                tempMaterial = new PhongMaterial();
                
                tempMaterial.setDiffuseColor(Color.BLUE);
@@ -54,10 +66,11 @@ public class GameView extends Application
                root.getChildren().add( nodes[i][j][k]);
                nodes[i][j][k].setTranslateX( i -2);
                nodes[i][j][k].setTranslateY( j -2);
-               nodes[i][j][k].setTranslateZ( k -2);
+               nodes[i][j][k].setTranslateZ( (k -2)*Math.sqrt(2));
                
                nodes[i][j][k].setMaterial( tempMaterial);
                nodes[i][j][k].setDrawMode(DrawMode.FILL);
+            	}
             }
          }
       }
@@ -66,9 +79,9 @@ public class GameView extends Application
       
       PerspectiveCamera camera = new PerspectiveCamera(true);
       camera.getTransforms().addAll (
-                                     new Rotate(30, Rotate.X_AXIS),
+                                     new Rotate(60, Rotate.X_AXIS),
                                      //new Rotate(-20, Rotate.X_AXIS));
-                                     new Translate(GameEngine.HEIGHT / 2 ,GameEngine.WIDTH /2 , -60));
+                                     new Translate(GameEngine.HEIGHT/2  ,GameEngine.WIDTH/3  , -40));
       
       camera.setFieldOfView(35);
       
@@ -83,9 +96,11 @@ public class GameView extends Application
       Scene scene = createScene();
       primaryStage.setScene(scene);
       primaryStage.show();
+      //System.out.println(GameView.levelX);
+      GameEngine game = new GameEngine("level1", 0 );
+      //GameEngine game = new GameEngine(GameView.levelX, 0 );
       
-      //GameEngine game = new GameEngine("level1", 0 );
-      GameEngine game = new GameEngine( );
+      //GameEngine game = new GameEngine( );
       primaryStage.addEventHandler( KeyEvent.KEY_PRESSED, event -> {
       switch (event.getCode()) {
         case UP:
@@ -118,21 +133,17 @@ public class GameView extends Application
         case A:
            game.xminus();
           break;
-        case DIGIT1:
-        case NUMPAD1:
-           game.setActive(1);
+        case C:
+        	game.mvdgl();
           break;
-        case DIGIT0:
-        case NUMPAD2:
-           game.setActive(0);
+        case Z:
+        	game.mvdgr();
           break;
+        case TAB:
+        	game.nextActive();
       }
-      try {
-		if(game.isFinish()) { System.exit(0);}
-	} catch (FileNotFoundException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
-	}
+      
+      if(game.isFinish()) { System.exit(0);}
     });
       
    }
